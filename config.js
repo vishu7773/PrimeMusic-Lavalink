7ad8a498f682bc5e9bfc49f302b4d0ae4905f65d
vishu7773 <atrone616@gmail.com>
2025-01-24 03:01:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://atrone764:Adityaloser1@atrone.hvxe3ua.mongodb.net/?retryWrites=true&w=majority&appName=Atrone",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
